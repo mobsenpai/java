a437e8074b7e8589465be33977ca640207e81b20
mobsenpai <work.velocity806@passinbox.com>
2025-07-13 22:18:16 +0530

    first commit

---
+++ Rect.java
@@ -0,0 +1,11 @@
+class Rect {
+  public static int area(int l, int b) {
+    return l * b;
+  }
+
+  public static void main(String[] args) {
+    int a = area(2, 5);
+    System.out.println("AREA: " + a);
+  }
+}
+;
